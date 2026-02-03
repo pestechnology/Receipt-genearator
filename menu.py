56890edcd01650bd6a0721c19619b35a239f14d4
prashanth152004 <prashanth152004@gmail.com>
2026-02-03 15:02:20 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,21 @@
+def show_menu():
+    menu = {
+        1: ("Burger", 50),
+        2: ("Pizza", 100),
+        3: ("Sandwich", 40),
+        4: ("Coffee", 30)
+    }
+
+    print("\n--- FOOD MENU ---")
+    for key, value in menu.items():
+        print(f"{key}. {value[0]} - Rs {value[1]}")
+
+    return menu
+
+
+def get_user_choice(menu):
+    choice = int(input("Choose item number: "))
+    quantity = int(input("Enter quantity: "))
+
+    item_name, price = menu[choice]
+    return item_name, price, quantity
